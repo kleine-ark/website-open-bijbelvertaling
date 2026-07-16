3feb792938ab1ff80aa4b3d6567f99d90975ee4d
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2026-07-16 05:40:49 +0200

    Bump service-worker VERSION (2026-07-16)

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Versionering: bump VERSION bij elke deploy om alle caches te vernieuwen.
  */
 
-const VERSION = '2026-06-29-galaten-afgerond'
+const VERSION = '2026-07-16-4baruch-chunkaudio'
 const SHELL_CACHE   = `shell-${VERSION}`;
 const DATA_CACHE    = `data-${VERSION}`;
 const LEXICON_CACHE = `lexicon-${VERSION}`;
