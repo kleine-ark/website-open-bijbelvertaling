8eaca58a1649468b8ad67308d115191014db302f
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2026-08-28 14:35:20 +0200

    Herstel atomaire woordnummers voor Mattheus 13

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Versionering: bump VERSION bij elke deploy om alle caches te vernieuwen.
  */
 
-const VERSION = 'v0.44.10';
+const VERSION = 'v0.44.11';
 const SHELL_CACHE   = `shell-${VERSION}`;
 const DATA_CACHE    = `data-${VERSION}`;
 const LEXICON_CACHE = `lexicon-${VERSION}`;
