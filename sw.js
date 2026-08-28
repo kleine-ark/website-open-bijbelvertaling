5b2f023de7629f80eb247e7a5aa7ee07fd04f728
Maarten Vroegindeweij <30430941+DutchSailor@users.noreply.github.com>
2026-08-28 15:24:28 +0200

    Herstel atomaire woordnummers voor Mattheus 11

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Versionering: bump VERSION bij elke deploy om alle caches te vernieuwen.
  */
 
-const VERSION = 'v0.44.12';
+const VERSION = 'v0.44.13';
 const SHELL_CACHE   = `shell-${VERSION}`;
 const DATA_CACHE    = `data-${VERSION}`;
 const LEXICON_CACHE = `lexicon-${VERSION}`;
